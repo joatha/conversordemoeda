0a75586b585c0d6ffd35a66e710f359485c80e55
Joathã Andrade <74799613+joatha@users.noreply.github.com>
2021-01-28 13:37:26 -0300

    Atualizando

---
+++ Conversor/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace Conversor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConvert1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(195, 94);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Converter";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(89, 94);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(100, 20);
+            this.txtBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dolar  /  Real";
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(88, 153);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(100, 20);
+            this.txtBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Real / Dolar";
+            // 
+            // btnConvert1
+            // 
+            this.btnConvert1.Location = new System.Drawing.Point(195, 152);
+            this.btnConvert1.Name = "btnConvert1";
+            this.btnConvert1.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert1.TabIndex = 5;
+            this.btnConvert1.Text = "Converter";
+            this.btnConvert1.UseVisualStyleBackColor = true;
+            this.btnConvert1.Click += new System.EventHandler(this.btnConvert1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(343, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "CONVERSOR DE MOEDA";
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(302, 182);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 7;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 217);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnConvert1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.btnConvert);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conversor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConvert1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Conversor/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+
+            double Valor1;
+            double Valor2 = 5.43;
+            double result;
+            
+
+            try
+            {
+                Valor1 = Convert.ToDouble(txtBox1.Text);
+               // Valor2 = Convert.ToDouble(txtBox2.Text);
+
+                result = Valor1 * Valor2;
+
+               
+
+                MessageBox.Show("O Dolar está sendo negóciado à: " + result);
+                txtBox1.Clear();
+
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Erro! Digite apenas numeros!");
+                txtBox1.Clear();
+                txtBox2.Clear();
+            }
+            
+
+
+            //lbText.Text += txtBox1.Text;
+        }
+
+        private void btnConvert1_Click(object sender, EventArgs e)
+        {
+            double ValorReal = 0.18;
+            double Valor2;
+            double result;
+            try
+            {
+                Valor2 = Convert.ToDouble(txtBox2.Text);
+                // Valor2 = Convert.ToDouble(txtBox2.Text);
+
+                result = Valor2 * ValorReal;
+
+                MessageBox.Show("O Real está sendo negociado à:" + result);
+                txtBox2.Clear();
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Erro! Digite apenas numeros!");
+                txtBox1.Clear();
+                txtBox2.Clear();
+            }
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            
+            Application.Exit();
+        }
+    }
+}
